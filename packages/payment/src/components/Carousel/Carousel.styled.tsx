77a73d28e3e02f8874cc4e43587657785f97c31f
airman5573 <rpf5573@kakao.com>
2023-09-05 10:49:39 +0900

    TEST

--- packages/payment/src/components/Carousel/Carousel.styled.tsx
+++ packages/payment/src/components/Carousel/Carousel.styled.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { theme } from '@styles/theme';
+import { theme } from '../../styles/theme';
 import arrowSrc from '@assets/arrow.svg';
 
 interface ContentListProps {
